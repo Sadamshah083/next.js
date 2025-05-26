1bd6961fd8d191d2810ffc3497f274df07778019
Sadamshah083 <Sadamshah083@gmail.com>
2025-05-26 20:31:31 +0500

    Updated-redux

---
+++ app/crud-redux/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useState } from "react";
+
+const page = () => {
+  const [form, setForm] = useState({ name: "", email: "" });
+  const [users, setUsers] = useState([]);
+  const [editId, setEditID] = useState(null);
+
+  const handleChange = (e: any) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = () => {
+    if (editId !== null) {
+      setUsers(users.map((user, index) => (index === editId ? form : user)));
+    } else {
+      setUsers([...users, form]);
+    }
+
+    setForm({ name: "", email: "" });
+  };
+  const handleEdit = (index) => {
+    const userToEdit = users[index];
+    setForm({ name: userToEdit.name, email: userToEdit.email });
+    setEditID(index);
+  };
+  return (
+    <div>
+      <input
+        name="name"
+        type="text"
+        placeholder="Name"
+        value={form.name}
+        onChange={handleChange}
+      />
+
+      <input
+        name="email"
+        type="text"
+        placeholder="Name"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <button onClick={handleSubmit}>
+        {editId !== null ? "update" : "Add"}
+      </button>
+
+      <div>
+        {users.map((e, index) => {
+          return (
+            <div key={index}>
+              {e.name} {e.email}
+              <button
+                className="bg-green-500"
+                onClick={() => {
+                  handleEdit(index);
+                }}
+              >
+                Edit
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default page;
