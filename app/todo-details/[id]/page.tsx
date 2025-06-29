bf7204a9b9dab85a53e1fa1c28ba0ebafa0be764
Sadamshah083 <Sadamshah083@gmail.com>
2025-06-29 14:04:49 +0500

    todoError debug

--- app/todo-details/[id]/page.tsx
+++ app/todo-details/[id]/page.tsx
@@ -1,25 +1,16 @@
 "use client";
-import { useSearchParams } from "next/navigation";
-
-interface PageProps {
-  params: { id: string };
-}
-
-const TodoDetailPage = ({ params }: PageProps) => {
+import { useParams, useSearchParams } from "next/navigation";
+const TodoDetailPage = () => {
+  const params = useParams();
   const searchParams = useSearchParams();
   const task = searchParams.get("task");
-
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4 text-green-700">Todo Detail</h1>
-      <p>
-        <strong>ID:</strong> {params.id}
-      </p>
       <p>
         <strong>Task:</strong> {task || "No task found"}
       </p>
     </div>
   );
 };
-
 export default TodoDetailPage;
